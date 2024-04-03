d4970dc4916cdf9995508d5f286ab9fdeff6210b
itsabdullah15 <itsabdullah.cg@gmail.com>
2024-04-03 22:46:55 +0530

    03042024 Abdullah

---
+++ mappingfile.py
@@ -0,0 +1,11 @@
+class IDENS:
+    Output_Folder_Location = '/home/abdullah/Desktop/Code/Python/PythonBasicProject/ClimateJusticeInternship/Output'
+    capctcha_folder_path = '/home/abdullah/Desktop/Code/Python/PythonBasicProject/ClimateJusticeInternship/CaptchaImg'
+    link = 'https://njdg.ecourts.gov.in/njdgnew/?p=main/index&state_code=7~26'
+    andaman_nicobar = 'https://njdg.ecourts.gov.in/njdgnew/?p=main/index&state_code=35~28'
+
+
+
+class XPATHS:
+    example_year_next_page_xpath = '//a[@class="paginate_button next" and @aria-controls="example_year"]'
+    cases_next_page_xpath = '//a[@class="paginate_button next" and @aria-controls="example_cases"]'
